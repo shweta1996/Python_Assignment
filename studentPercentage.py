e9aee261de76c75aa221731d908b9dcf8f87e8aa
shweta1996 <Kasaudhansweta@gmail.com>
2026-05-04 11:26:29 +0530

    Calculate Student % done

---
+++ studentPercentage.py
@@ -0,0 +1,6 @@
+# Calculate student percentage Input: obtain_marks = 430, out_of = 500 
+# Output: Percentage = ?
+obtain_marks = 430
+out_of = 500 
+percentage=(obtain_marks / out_of)*100
+print(percentage)
